d6089a52438f495b19fca65887f33a7440a190e0
DenisMelnik777 <129406422+DenisMelnik777@users.noreply.github.com>
2023-05-22 19:31:04 +0300

    Add files via upload

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,43 +1,6 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
-from src.item import Item
-
-
-@pytest.fixture
-def Item_test():
-    return Item("Ноутбук", 20000, 5)
-
-
-def test_init(Item_test):
-    assert Item_test.name == "Ноутбук"
-    assert Item_test.price == 20000
-    assert Item_test.quantity == 5
-
-
-def test_calculate_total_price(Item_test):
-    assert Item_test.calculate_total_price() == 100000
-
-
-def test_apply_discount(Item_test):
-    Item_test.pay_rate = 1.0
-    Item_test.apply_discount()
-    assert Item_test.price == 20000
-    assert Item_test.calculate_total_price() == 100000.0
-
-def test_name_setter():
-    item = Item('СуперСмартфон', 10000, 1)
-    item.name = 'Ноутбук'
-    assert item.name == 'Ноутбук'
-
-def test_instantiate_from_csv():
-    item1 = Item('Смартфон', 10000, 1)
-    item1.instantiate_from_csv()
-    assert len(Item.all) == 10
-    assert Item.all[0].name == 'Смартфон'
-
-def test_string_to_number():
-    item = Item('Смартфон', 10000, 1)
-    assert isinstance(item.string_to_number(item.quantity), int)
+from src.item import Item, InstantiateCSVError
 
 
 def test_repr():
@@ -53,3 +16,64 @@ def test_str():
     assert str(item1) == 'Смартфон'
     assert str(item2) == 'Ноутбук'
 
+
+def test_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    assert item1.total_price() == 200000
+    assert item2.total_price() == 100000
+
+
+def test_apply_pay_rate():
+    Item.discount = 0.9  # установка скидки на все товары
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    assert item1.price == 10000  # ожидаемая цена со ставкой оплаты
+    assert item2.price == 20000  # ожидаемая цена со ставкой оплаты
+
+
+def test_apply_pay_rate_negative_pay_rate():
+    Item.discount = -0.1  # отрицательная скидка
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    item1.apply_pay_rate(0.1)
+    assert item1.price == 9000
+    assert item2.price == 20000
+
+
+def test_apply_pay_rate_positive_pay_rate():
+    Item.pay_rate = 0.5  # положительный коэффициент оплаты
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    item1.apply_pay_rate(Item.pay_rate)  # передать pay_rate как аргумент
+    assert item1.price == 5000 and item2.price == 20000
+
+
+def test_name_setter():
+    item = Item('СуперСмартфон', 10000, 1)
+    item.name = 'Ноутбук'
+    assert item.name == 'Ноутбук'
+
+
+def test_instantiate_from_csv():
+    item1 = Item('Смартфон', 10000, 1)
+    item1.instantiate_from_csv()
+    assert len(Item.all) == 10
+    assert Item.all[0].name == 'Смартфон'
+
+
+def test_string_to_number():
+    item = Item('Смартфон', 10000, 1)
+    assert isinstance(item.string_to_number(item.quantity), int)
+
+
+def test_exception_instantiate_from_csv():
+    Item.file_name = '123'
+    with pytest.raises(FileNotFoundError) as e:
+        Item.instantiate_from_csv()
+    assert str(e.value) == f'Отсутствует файл {Item.file_name}'
+
+    Item.file_name = 'items_test.csv'
+    with pytest.raises(InstantiateCSVError) as e:
+        Item.instantiate_from_csv()
+    assert str(e.value) == f'Файл {Item.file_name} поврежден'
